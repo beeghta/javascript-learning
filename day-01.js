decf1154c525a823b2795c22a96787a79b4dca17
Beata <bita.zarabie@gmail.com>
2026-08-19 18:53:11 +0330

    Add first JavaScript exercise

--- day-01.js
+++ day-01.js
@@ -1,1 +1,19 @@
-console.log("Hello, JavaScript!");// JavaScript source code
+const neuronName = "Excitatory Neuron";
+let membranePotential = -70;
+const threshold = -55;
+let stimulus = 20;
+let newPotential = membranePotential + stimulus;
+
+
+function checkShoot() {
+    if (newPotential > threshold) {
+        console.log("Strong firing!");
+    } else if (newPotential === threshold) {
+        console.log("Threshold reached.");
+    } else {
+        console.log("No firing");
+    }
+}
+checkShoot();
+console.log(newPotential);
+console.log(neuronName);
