b5a63c7b940ccb5694373d19d5db0703197aa67b
Beata <bita.zarabie@gmail.com>
2026-08-22 16:15:56 +0330

    Day 4

---
+++ day-4/README.md
@@ -0,0 +1,47 @@
+# Day 4 — Array Validation \& Data Processing
+
+
+
+# 
+
+
+
+###### **Topics Learned:**
+
+* Object methods
+* Optional Chaining
+* Immutable Updates
+* Error Handling
+* try/catch/finally
+* throw 
+* Custom Error
+* Filtering arrays based on conditions
+* Counting filtered results with .length
+* Creating structured reports
+* Processing validation results
+* ###### Advanced Array Methods
+
+
+
+##### **Project**
+
+Neuron Validation Report :In this exercise, I processed validation results from a neuron analysis system.
+
+
+
+###### The program:
+
+
+
+Filters valid results
+
+Filters invalid results
+
+Counts valid neurons
+
+Counts invalid neurons
+
+Creates a structured report object
+
+
+
---
+++ day-4/main.js
@@ -0,0 +1,51 @@
+﻿const neurons = [
+    { name: "Neuron A", potential: -70, threshold: -55 },
+    { name: "Neuron B", potential: -50, threshold: "-55" },
+    { name: "Neuron C", potential: -60, threshold: -55 }
+];
+class InvalidNeuronError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "InvalidNeuronError";
+    }
+}
+const validateNeuron = neuron => {
+    if (
+        typeof neuron.potential !== "number" ||
+        typeof neuron.threshold !== "number" ||
+        typeof neuron.name !== "string" 
+    ) {
+
+        throw new InvalidNeuronError(
+            "Neuron potential and threshold must be numbers And Neuron name must be a string"
+        );
+
+    }
+    return true;
+};
+
+const validateNeurons = neurons => {
+    const validationResults = [];
+
+    neurons.forEach(neuron => {
+
+        try {
+            validateNeuron(neuron);
+            console.log(neuron.name, "is valid");
+            validationResults.push(`${neuron.name}: Valid`);
+
+        } catch (error) {
+            if (error instanceof InvalidNeuronError) {
+                console.log(neuron.name, "is Invalid");
+                validationResults.push(`${neuron.name}: Invalid`);
+            }
+            else {
+                console.log(`Unknown error: ${error.message}`);
+            }
+        }
+    });
+    return validationResults;
+};
+const results = validateNeurons(neurons);
+
+console.log(results);
