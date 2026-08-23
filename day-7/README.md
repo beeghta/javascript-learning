75a1cd6a3002b82965168e6e1bdea4c9f6b5e961
Beata <bita.zarabie@gmail.com>
2026-08-23 17:14:55 +0330

    Day 7 — Asynchronous JavaScript

---
+++ day-7/README.md
@@ -0,0 +1,9 @@
+## Day 7 — Asynchronous JavaScript
+
+
+
+###### **Topics Learned:**
+
+* Synchronous
+* 
+
---
+++ day-7/main.js
@@ -0,0 +1,27 @@
+﻿const neurons = [
+    { name: "Neuron A", potential: -70, threshold: -55 },
+    { name: "Neuron B", potential: -50, threshold: -55 },
+    { name: "Neuron C", potential: -40, threshold: -55 }
+];
+
+const simulateNeuron = neuron => {
+    console.log(`${neuron.name} is being checked...`);
+
+    if (neuron.potential >= neuron.threshold) {
+        setTimeout(() => {
+            console.log(`${neuron.name} fired!`);
+        }, 1000);
+    } else {
+        setTimeout(() => {
+            console.log(`${neuron.name} did not fire.`);
+        }, 2000);
+    }
+};
+
+console.log("Neuron simulation started");
+
+neurons.forEach(neuron => {
+    simulateNeuron(neuron);
+});
+
+console.log("Neuron simulation finished");
